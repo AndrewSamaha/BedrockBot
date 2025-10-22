20179f7bdfd732bf24d60e6925210bbbef261c0c
Andrew Samaha <andrew.samaha@gmail.com>
2025-10-22 18:54:50 -0400

    feat: FIRST MOVE

--- src/lib/client/handlers/start_game.ts
+++ src/lib/client/handlers/start_game.ts
@@ -1,28 +1,25 @@
 import { gameState } from '@/lib/GameState';
 import { log } from '@/lib/log';
 
-function sendCommand(client: any, command: string) {
-  client.queue('command_request', {
-    command,                     // "fill 0 64 0 10 64 10 stone"
-    origin: {
-      type: 0,                   // 0 = player
-      uuid: client.profile?.uuid || client.uuid || '00000000-0000-0000-0000-000000000000',
-      request_id: ''             // optional
-    },
-    internal: false,
-    interval: 0
-  })
-}
-
 const startGame = {
   name: 'start_game' as const,
   fn: (packet: any, client: any) => {
     log({ packet });
     log({ server_authoritative_movement: packet.server_authoritative_movement })
     gameState.startGame(client, packet);
-    log({ sending: 'set_local_player_as_initialized', runtime_entity_id: packet.runtime_entity_id });
 
+    log({ sending: 'serverbound_loading_screen type 1' });
+    client.queue('serverbound_loading_screen', { "type": 1 });
+    log({ sending: 'serverbound_loading_screen type 2' });
+    client.queue('serverbound_loading_screen', { "type": 2 });
+    log({ sending: 'interact' });
+    client.queue('interact', {
+      "action_id": "mouse_over_entity",
+      "target_entity_id": 0n,
+      "position": { "x": 0, "y": 0, "z": 0 }
+    });
     // Send the correct packet with proper field name and BigInt type
+    log({ sending: 'set_local_player_as_initialized', runtime_entity_id: packet.runtime_entity_id });
     client.queue('set_local_player_as_initialized', {
       runtime_entity_id: packet.runtime_entity_id // Keep as BigInt (varint64)
     });
