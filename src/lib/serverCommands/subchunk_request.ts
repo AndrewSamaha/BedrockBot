c5c101d0fe787bb1868f31c6bc7399526efec524
Andrew Samaha <andrew.samaha@gmail.com>
2025-12-01 17:28:16 -0500

    chore: Cleans up subchunk_request

--- src/lib/serverCommands/subchunk_request.ts
+++ src/lib/serverCommands/subchunk_request.ts
@@ -7,25 +7,6 @@ import { type LookVector } from "../types";
 import { type GameState } from "@/lib/GameState";
 import { log } from '@/lib/log';
 
-// const command = (client: Client, gS: GameState) => {
-//   const params = {
-//     dimension: 0,
-//     origin: {
-//       x: -2,
-//       y: 0,
-//       z: -2
-//     },
-//     requests: [
-//       {
-//         dx: 5,
-//         dy: -4,
-//         dz: 0
-//       }
-//     ]
-//   }
-//   log({ subchunk_request: params });
-//   client.queue('subchunk_request', params);
-// }
 const command = (client: Client, gS: GameState) => {
   const pos = gS.playerPosition;
   if (!pos) {
@@ -66,7 +47,6 @@ const command = (client: Client, gS: GameState) => {
     requests,
   };
 
-  log({ subchunk_request: params });
   client.queue('subchunk_request', params);
 };
 export default command;
